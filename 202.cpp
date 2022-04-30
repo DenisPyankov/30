a8bb1ccc4c02fcf525b9e1b251281f49ee3dbefc
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-04-30 10:40:16 +0500

    Add files via upload

---
+++ 202.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+    string S, T;
+    in >> S >> T;
+    int start = 0;
+    int count = 0;
+    int pos = 0;
+    for (;;) {
+        pos = S.find(T.c_str(), start);
+        if (pos != -1) {
+            start = pos + 1;
+            out << pos << " ";
+        }
+        else {
+            break;
+        }
+    }
+
+
+}
